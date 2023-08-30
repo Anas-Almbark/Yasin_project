36ee1f8b7033d9cfc9a72ae6c5ba12aa9c0c8661
Anas-Almbark <coder.anas2@gmail.com>
2023-08-30 21:19:53 +0300

    Finish the work for the about section

--- js/app.js
+++ js/app.js
@@ -1,18 +1,32 @@
-// For header Logo
+// Select Elements
+let listIcon = document.querySelector(".list-icon i");
+let nav = document.querySelector(".nav");
 let header = document.querySelector(".header");
-if (document.body.clientWidth <= 500) {
-  header.classList.add("mobile");
-} else {
-  header.classList.remove("mobile");
-}
-
 let mainLogo = document.querySelector(".main-logo");
+let about = document.querySelector(".about");
+// For Background Header
+document.body.clientWidth <= 500
+  ? header.classList.add("mobile")
+  : header.classList.remove("mobile");
+// For Nav Logo
 document.body.clientWidth <= 1280
   ? (mainLogo.src = "./images/logo.png")
   : (mainLogo.src = "./images/logo2.png");
+
+window.onscroll = function () {
+  // For Fixed Nav When Window Scroll 200
+  if (window.scrollY > 100) {
+    nav.classList.add("scroll-down");
+    mainLogo.src = "./images/logo.png";
+  } else {
+    mainLogo.src = "./images/logo2.png";
+    nav.classList.remove("scroll-down");
+  }
+  window.scrollY >= about.offsetTop - 550 // For About
+    ? (about.style.top = `${-150}px`)
+    : (about.style.top = `${0}px`);
+};
 // For Open List Nav
-let listIcon = document.querySelector(".list-icon i");
-let nav = document.querySelector(".nav");
 listIcon.addEventListener("click", () => {
   listIcon.classList.toggle("fa-xmark");
   nav.classList.toggle("open-nav");
