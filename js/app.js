d7fdd1e69a02ab9d9f41d74abeab9274bfd09af3
Anas-Almbark <coder.anas2@gmail.com>
2023-11-26 16:13:57 +0300

    anthing

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ document.body.clientWidth <= 1280
 
 window.onscroll = function () {
   // For Fixed Nav When Window Scroll 200
-  if (window.scrollY > 100) {
+  if (window.scrollY > 60) {
     nav.classList.add("scroll-down");
     mainLogo.src = "./images/logo.png";
   } else if (window.scrollY < 100 && document.body.clientWidth <= 1280) {
@@ -42,3 +42,48 @@ document.querySelectorAll(".header nav ul li").forEach((li) => {
     }
   };
 });
+
+// for slider
+let allImagesSlider = [
+  "../images/my_work/00.jpg",
+  "../images/my_work/01.jpg",
+  "../images/my_work/02.jpg",
+  "../images/my_work/03.jpg",
+  "../images/my_work/04.jpg",
+  "../images/my_work/05.jpg",
+  "../images/my_work/06.jpg",
+  "../images/my_work/07.jpg",
+  "../images/my_work/08.jpg",
+  "../images/my_work/09.jpg",
+  "../images/my_work/10.jpg",
+  "../images/my_work/11.jpg",
+  "../images/my_work/12.jpg",
+  "../images/my_work/13.jpg",
+  "../images/my_work/14.jpg",
+  "../images/my_work/15.jpg",
+  "../images/my_work/16.jpg",
+  "../images/my_work/17.jpg",
+  "../images/my_work/18.jpg",
+  "../images/my_work/19.jpg",
+  "../images/my_work/20.jpg",
+  "../images/my_work/21.jpg",
+  "../images/my_work/22.jpg",
+  "../images/my_work/23.jpg",
+  "../images/my_work/24.jpg",
+  "../images/my_work/25.jpg",
+  "../images/my_work/26.jpg",
+  "../images/my_work/27.jpg",
+  "../images/my_work/28.jpg",
+  "../images/my_work/29.jpg",
+];
+let swiper1 = document.querySelector(".mySwiper");
+let swiper2 = document.querySelector(".mySwiper2");
+for (let i = 0; i < allImagesSlider.length; i++) fetchImg(i);
+function fetchImg(i) {
+  let swiperSlide = document.createElement("swiper-slide");
+  let swiperSlideImg = document.createElement("img");
+  swiperSlideImg.src = allImagesSlider[i];
+  swiperSlide.appendChild(swiperSlideImg);
+  swiper2.appendChild(swiperSlide);
+  swiper1.appendChild(swiperSlide.cloneNode(true));
+}
